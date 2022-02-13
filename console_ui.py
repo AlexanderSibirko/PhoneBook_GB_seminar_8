7cd2d25403cd410a5a1974c81c13be6f258185e9
Alexander <Alexandersibirko@yandex.ru>
2022-02-13 10:57:21 +0300

    added basic structure

---
+++ console_ui.py
@@ -0,0 +1,4 @@
+# Команды командной строки
+# 1) Загрузить из (Формат: pb_import --format file.name)  formats = [txt, csv, json, xml]
+# 2) Выгрузить из (Формат: pb_export --format file.name)  formats = [txt, csv, json, xml]
+# 3)
