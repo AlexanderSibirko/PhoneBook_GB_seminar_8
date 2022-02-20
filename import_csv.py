3f4610845f8f768e79b4f96cf9ec1ae5f30c96a0
Alexander <Alexandersibirko@yandex.ru>
2022-02-20 09:54:05 +0300

    changed delimiter to , because export make csv with , delimiter

--- import_csv.py
+++ import_csv.py
@@ -1,8 +1,7 @@
 import csv
 
-
 def imp_csv(path):
-    with open(path) as f:
+    with open(path, encoding='UTF-8') as f:
         data = []
-        data = list(csv.reader(f, delimiter=";"))
+        data = list(csv.reader(f, delimiter=","))
     return data
