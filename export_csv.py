0afe7f186583f19be337eaebef2b36ce989b9590
Alexander <Alexandersibirko@yandex.ru>
2022-02-20 09:53:21 +0300

    added encoding to work with russian letters

--- export_csv.py
+++ export_csv.py
@@ -3,6 +3,6 @@ import csv
 
 def exp_csv(path, data):
 
-    with open(path, "w", newline='') as f:
-        writer = csv.writer(f)
+    with open(path, "w", newline='', encoding='UTF-8') as f:
+        writer = csv.writer(f, dialect='excel')
         writer.writerows(data)
