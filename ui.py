3d24c7491d05d3a8bd27f7e24fc6a1fe4d8808c2
Alexander <Alexandersibirko@yandex.ru>
2022-02-20 09:58:40 +0300

    added print() method and exit command

--- ui.py
+++ ui.py
@@ -1,6 +1,6 @@
 def what_operation():
-    operation_set = ['import', 'export', 'add', 'remove']
-    input_operation = input('Введите тип операции(import/export/add/remove): ')
+    operation_set = ['import', 'export', 'add', 'remove', 'exit']
+    input_operation = input('Введите тип операции(import/export/add/remove/exit): ')
     if input_operation == operation_set[0] or input_operation == operation_set[1]:
         format, path = input('txt/csv: '), input('path: ')
         return [input_operation, format, path]
@@ -9,4 +9,8 @@ def what_operation():
         return adding_data
     if input_operation == operation_set[3]:
         return [input_operation, input('Фамилия удаляемого: ')]
-    
+    if input_operation == operation_set[4]:
+        return [input_operation]
+
+def print_msg(message):
+    print(message)
